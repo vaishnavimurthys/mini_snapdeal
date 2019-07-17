f589d58cfce7388e8b64450865f8b679a10976f8
vaishnavimurthys <svaishnavi1973@gmail.com>
2019-07-17 10:15:11 +0530

    added mini-snapdeal

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
 from flask import Flask,render_template,request,redirect,url_for,session
+from models.model1 import user_exists,save_user,add_product,product_exists
+
 
 app = Flask(__name__)
 app.secret_key='hello'
@@ -13,34 +15,76 @@ def about():
 
 	return render_template('about.html',title='about')
 
-@app.route('/contact')	 
+@app.route('/contact')
 def contact():
 
 	return render_template('contact.html',title='contact')
 
 
-@app.route('/login',methods=['GET','POST'])	
+@app.route('/login',methods=['GET','POST'])
 def login():
-	if request.method == 'POST':
-		users={'user1':'123','user2':'234','user3':'1234','user4':'2345'} 
-		username = request.form['username']
-		password = request.form['password']
+		if request.method == 'POST':
+			username = request.form['username']
+			password = request.form['password']
 
-		if username not in users:
-			return "user doesnt exist.Go back and enter valid username "
+			result=user_exists(username)
+			if result:
 
-		if users[username]!=password:
-			return "password doesnt match"
-	
-		session['username'] = username
+
+				if result['password']!=password:
+					return "password doesnt match.Go back and enter valid username "
+
+				session['username'] = username
+				session['c_type']=result['c_type']
+				return redirect(url_for('home'))
+			return "username doesnt exists"
 		return redirect(url_for('home'))
+
+
+@app.route('/signup',methods=['GET','POST'])
+def signup():
+
+	if request.method=='POST':
+
+		user_info={}
+
+		user_info['username']=request.form['username']
+		user_info['password']=request.form['password1']
+		password2=request.form['password2']
+		user_info['c_type']=request.form['type']
+		if user_info['c_type']=='buyer':
+			user_info['cart']= []
+		if user_exists(user_info['username']):
+			return"username already exists"
+		if user_info['password']!=password2:
+			return "passwords dont match"
+		save_user(user_info)
+		return redirect(url_for('home'))
+
+
+
+
 	return redirect(url_for('home'))
-		
-		
+
+@app.route('/products', methods=['GET','POST'])
+def products():
+	if request.method=='POST':
+		product_info={}
+		product_info['name']=request.form['name']
+		product_info['price']=int(request.form['price'])
+		product_info['description']=request.form['description']
+		product_info['seller']=session['username']
+
+		if product_exists(product_info['name']):
+			return "product exists"
+		add_product(product_info)
+		return "product added ! check your db"
+	return(redirect(url_for('home')))
+
 @app.route('/logout')
 def logout():
-	session.clear()
-	return redirect(url_for('home'))
+			session.clear()
+			return redirect(url_for('home'))
 
 
 app.run(debug=True)
