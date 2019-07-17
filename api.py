c7aa7c8ca9939d69f093afec139cf6083aae16ef
vaishnavimurthys <svaishnavi1973@gmail.com>
2019-07-17 10:44:28 +0530

    latest

--- api.py
+++ api.py
@@ -1,12 +1,12 @@
 from flask import Flask,render_template,request,redirect,url_for,session
-from models.model1 import user_exists,save_user,add_product,product_exists
-
-
+from models.model1 import user_exists,save_user,product_exists,add_product,products_list,remove_from_db,add_to_cart,cart_info,remove_from_cart
 app = Flask(__name__)
-app.secret_key='hello'
+app.secret_key = 'hello'
 
 @app.route('/')
+
 def home():
+
 
 	return render_template('home.html',title='home')
 
@@ -20,71 +20,139 @@ def contact():
 
 	return render_template('contact.html',title='contact')
 
-
 @app.route('/login',methods=['GET','POST'])
 def login():
-		if request.method == 'POST':
-			username = request.form['username']
-			password = request.form['password']
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		result = user_exists(username)
 
-			result=user_exists(username)
-			if result:
+		if result:
 
+			if result['password']!=password:
 
-				if result['password']!=password:
-					return "password doesnt match.Go back and enter valid username "
+				return "password doesn't match. Go back and re-enter the password"
 
-				session['username'] = username
-				session['c_type']=result['c_type']
-				return redirect(url_for('home'))
-			return "username doesnt exists"
-		return redirect(url_for('home'))
+			session['username'] = username
 
+			session['c_type'] = result['c_type']
+
+			return(redirect(url_for('home')))
+
+		return "username doesn't exist"
+
+	return redirect(url_for('home'))
 
 @app.route('/signup',methods=['GET','POST'])
 def signup():
 
-	if request.method=='POST':
-
+	if request.method == 'POST':
 		user_info={}
 
-		user_info['username']=request.form['username']
-		user_info['password']=request.form['password1']
-		password2=request.form['password2']
-		user_info['c_type']=request.form['type']
+		user_info['username'] = request.form['username']
+
+		user_info['password'] = request.form['password1']
+
+		password2 = request.form['password2']
+
+		user_info['c_type'] = request.form['type']
+
 		if user_info['c_type']=='buyer':
-			user_info['cart']= []
+
+			user_info['cart']=[]
+
 		if user_exists(user_info['username']):
-			return"username already exists"
+
+			return "user already exists. Enter another username"
+
 		if user_info['password']!=password2:
-			return "passwords dont match"
+
+			return "passwords don't match. Try again"
+
 		save_user(user_info)
+
+		session['username'] = username
+
+		session['c_type'] = result['c_type']
+
 		return redirect(url_for('home'))
-
-
-
 
 	return redirect(url_for('home'))
 
-@app.route('/products', methods=['GET','POST'])
+@app.route('/products',methods=['GET','POST'])
 def products():
-	if request.method=='POST':
+
+	if request.method == 'POST':
+
 		product_info={}
-		product_info['name']=request.form['name']
-		product_info['price']=int(request.form['price'])
-		product_info['description']=request.form['description']
-		product_info['seller']=session['username']
+
+		product_info['name'] = request.form['name']
+
+		product_info['price'] = int(request.form['price'])
+
+		product_info['description'] = request.form['description']
+
+		product_info['seller'] = session['username']
 
 		if product_exists(product_info['name']):
+
 			return "product exists"
+
 		add_product(product_info)
-		return "product added ! check your db"
-	return(redirect(url_for('home')))
+
+		return redirect(url_for('products'))
+
+	products = products_list()
+
+	return render_template('products.html',products=products)
+
+@app.route('/remove_products',methods=['GET','POST'])
+def remove_products():
+
+	if request.method == 'POST':
+
+		name = request.form['name']
+
+		remove_from_db(name)
+
+		return redirect(url_for('products'))
+
+	return redirect(url_for('products'))
+
+
+@app.route('/cart',methods=['GET','POST'])
+def cart():
+
+	if request.method == 'POST':
+
+		name = request.form['name']
+
+		add_to_cart(name)
+
+		return redirect(url_for('cart'))
+
+	products = cart_info()	
+
+	return render_template('cart.html',products=products)
+
+@app.route('/remove_from_cart',methods=['GET','POST'])
+def remove_cart():
+
+	if request.method == 'POST':
+
+		product = request.form['name']
+
+		remove_from_cart(product)
+
+		return redirect(url_for('cart'))
+
+	return redirect(url_for('cart'))
 
 @app.route('/logout')
 def logout():
-			session.clear()
-			return redirect(url_for('home'))
+	session.clear()
 
+	return redirect(url_for('home'))
 
 app.run(debug=True)
+
--- models/model1.py
+++ models/model1.py
@@ -1,22 +1,24 @@
 from pymongo import MongoClient
-
-client=MongoClient()
-db =client['snapdeal']
-
+from flask import session
+client = MongoClient()
+db = client['amazon']
 def user_exists(username):
-	query = {"username":username}
+
+	query = {'username':username}
+
 	result = db['users'].find_one(query)
+
 	if bool(result):
+
 		return result
+
 	return False
-	
 
 def save_user(user_info):
 	db['users'].insert_one(user_info)
 
-
 def product_exists(product_name):
-	query = {"name":product_name}
+	query = {'name':product_name}
 	result = db['products'].find_one(query)
 	if bool(result):
 		return result
@@ -24,3 +26,38 @@ def product_exists(product_name):
 
 def add_product(product_info):
 	db['products'].insert_one(product_info)
+
+def products_list():
+	if session['c_type'] == 'buyer':
+		result = db['products'].find({})
+		return result
+	query = {"seller":session['username']}
+	result = db['products'].find(query)
+	return result
+
+def remove_from_db(name):
+	query = {"name":name}
+	db['products'].remove(query)
+
+def add_to_cart(product):
+	query = {"username":session['username']}
+	action = {"$addToSet":{"cart":{"$each":[product]}}}
+	db['users'].update(query,action)
+
+def cart_info():
+	query1 = {"username":session['username']}
+	names = db['users'].find_one(query1)['cart']
+	info = []
+	for name in names:
+		query2 = {"name":name}
+		result = db['products'].find_one(query2)
+		info.append(result)
+
+	return info 
+
+
+def remove_from_cart(product):
+	query = {"username":session['username']}
+	action ={"$pull":{"cart":product}}
+	db['users'].update(query,action)
+
